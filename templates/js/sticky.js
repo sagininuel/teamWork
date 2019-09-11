c0edca97288be64b7c31ab5a0872d8d149be87b0
sagininuel <sagininuel@gmail.com>
2019-09-11 16:18:03 +0300

    feature(comment-an-article): modify markup to set up comment otpion [Start #168257834]

---
+++ templates/js/sticky.js
@@ -0,0 +1,14 @@
+var nav = document.getElementById("nav");
+
+window.onscroll = function() {
+    if (window.pageYOffset > 100) {
+        nav.style.position = "fixed";
+        nav.style.top = 0;
+        nav.style.background = "rgba(0,0,0,0.85";
+        nav.style.width = "100%";
+    } else {
+        // nav.style.position = "absolute";
+        nav.style.position = "relative"; //fixed
+        nav.style.top = 100;
+    }
+};
